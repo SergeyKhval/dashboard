84ba5e6a8a1702c5378e6a87fd8e0ca4a2367ca8
Sergey Khval <sergeykhval@gmail.com>
2016-05-20 21:21:43 +0300

    chore: remove unneded dependencies

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -7,11 +7,8 @@ import { MainController } from './main/main.controller';
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
-import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 
 angular.module('dashboard', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngRoute', 'ui.bootstrap', 'toastr'])
-  .constant('malarkey', malarkey)
-  .constant('moment', moment)
   .config(config)
   .config(routerConfig)
   .run(runBlock)
@@ -19,4 +16,3 @@ angular.module('dashboard', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 
   .service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
   .directive('acmeNavbar', NavbarDirective)
-  .directive('acmeMalarkey', MalarkeyDirective);
