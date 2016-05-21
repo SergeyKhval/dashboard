a16b59068eed970fbed73f7a15f73c5db9ef07d4
Sergey Khval <sergeykhval@gmail.com>
2016-05-21 18:21:02 +0300

    feat: add employees service

---
+++ src/app/employees/employees.service.js
@@ -0,0 +1,5 @@
+export class EmployeesService {
+  constructor() {
+
+  }
+}
--- src/app/index.module.js
+++ src/app/index.module.js
@@ -3,6 +3,7 @@ import {routerConfig} from './index.route';
 import {runBlock} from './index.run';
 import {MainController} from './main/main.controller';
 import {EmployeesController} from './employees/employees.controller';
+import {EmployeesService} from './employees/employees.service';
 import {IssuesController} from './issues/issues.controller';
 import {AnalyticsController} from './analytics/analytics.controller';
 import {StatusbarDirective} from '../app/components/statusbar/statusbar.js';
@@ -18,12 +19,14 @@ angular.module('dashboard',
     'ngAria',
     'ngRoute',
     'ui.bootstrap',
-    'angularfire'
+    'firebase'
   ])
   .config(config)
   .config(routerConfig)
   .run(runBlock)
+  .constant('Firebase', 'https://dashboard-7f32e.firebaseio.com/')
   .controller('MainController', MainController)
+  .service('EmployeesService', EmployeesService)
   .controller('EmployeesController', EmployeesController)
   .controller('IssuesController', IssuesController)
   .controller('AnalyticsController', AnalyticsController)
