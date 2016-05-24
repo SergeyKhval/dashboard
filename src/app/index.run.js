716237c844e38bdd3f7c582891638646cac24b0a
Sergey Khval <sergeykhval@gmail.com>
2016-05-24 22:34:11 +0300

    fix: slideout now properly hides on location change

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -1,4 +1,13 @@
-export function runBlock ($log) {
+export function runBlock($log, $rootScope) {
   'ngInject';
+
+  function broadCastSlideout() {
+    $rootScope.$broadcast('slideout', false);
+  }
+
+  let locationChange = $rootScope.$on('$locationChangeStart', broadCastSlideout);
+
+  $rootScope.$on('$destroy', locationChange);
+
   $log.debug('runBlock end');
 }
