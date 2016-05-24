714cac9d42163ea02e1f1ff7ae13a7aa0d0c9ab6
Sergey Khval <sergeykhval@gmail.com>
2016-05-24 23:23:13 +0300

    feat: add modal to add new employees

--- src/app/employees/employees.controller.js
+++ src/app/employees/employees.controller.js
@@ -1,6 +1,52 @@
 export class EmployeesController {
-  constructor() {
+  constructor($log, $uibModal) {
     'ngInject';
 
+    this.$uibModal = $uibModal;
+    this.$log = $log;
+
+    this.items = ['item1', 'item2', 'item3'];
+  }
+
+  openAddEmployeeModal(size) {
+    let modalInstance = this.$uibModal.open({
+      templateUrl: 'myModalContent.html',
+      controller: EmployeesModalController,
+      controllerAs: 'emc',
+      size: size,
+      resolve: {
+        items: () => {
+          return this.items;
+        }
+      }
+    });
+
+    modalInstance.result
+      .then(selectedItem => {
+        this.selected = selectedItem;
+      })
+      .catch(() => {
+        this.$log.info('Modal dismissed at: ' + new Date());
+      });
+  }
+}
+
+class EmployeesModalController {
+  constructor($uibModalInstance, items) {
+    'ngInject';
+
+    this.$uibModalInstance = $uibModalInstance;
+    this.items = items;
+    this.selected = {
+      item: this.items[0]
+    };
+  }
+
+  ok() {
+    this.$uibModalInstance.close(this.selected.item);
+  }
+
+  cancel() {
+    this.$uibModalInstance.dismiss('cancel');
   }
 }
