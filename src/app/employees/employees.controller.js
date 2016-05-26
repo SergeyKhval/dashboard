71a1846401fc1d27600957b1344adfdcf5b2cccf
Sergey Khval <sergeykhval@gmail.com>
2016-05-26 23:31:37 +0300

    feat: add employees accordion

--- src/app/employees/employees.controller.js
+++ src/app/employees/employees.controller.js
@@ -5,9 +5,12 @@ const _employeesService = new WeakMap();
 const _uibModalInstance = new WeakMap();
 
 export class EmployeesController {
-  constructor($log, $uibModal) {
+  constructor($log, $uibModal, EmployeesService) {
     _uibModal.set(this, $uibModal);
     _log.set(this, $log);
+    _employeesService.set(this, EmployeesService);
+
+    this.employees = _employeesService.get(this).employees;
   }
 
   openAddEmployeeModal(size) {
@@ -36,6 +39,6 @@ class EmployeesModalController {
   }
 }
 
-EmployeesController.$inject = ['$log', '$uibModal'];
+EmployeesController.$inject = ['$log', '$uibModal', 'EmployeesService'];
 EmployeesModalController.$inject = ['$log', '$uibModalInstance', 'EmployeesService'];
 
--- src/app/employees/employees.service.js
+++ src/app/employees/employees.service.js
@@ -1,11 +1,26 @@
+'use strict';
+
+const _employeesRef = new WeakMap();
+const _firebaseArray = new WeakMap();
+
 export class EmployeesService {
   constructor(Ref, $firebaseArray) {
-    this.$firebaseArray = $firebaseArray;
-    this.employeesRef = Ref.child('employees');
+    _employeesRef.set(this, Ref.child('employees'));
+    _firebaseArray.set(this, $firebaseArray);
+
+    this.employees = _firebaseArray.get(this)(_employeesRef.get(this));
   }
 
-  addEmployee(employee){
-    return this.$firebaseArray(this.employeesRef).$add(employee);
+  addEmployee(employee) {
+    let city = employee.city.toLowerCase();
+
+    let employeeObj = {
+      firstName: employee.firstName,
+      lastName: employee.lastName,
+      position: employee.position
+    };
+
+    return _firebaseArray.get(this)(_employeesRef.get(this).child(city)).$add(employeeObj);
   }
 }
 
