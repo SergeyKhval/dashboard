b04eeebbe218defcc50654718376e184f2d7afda
Sergey Khval <sergeykhval@gmail.com>
2016-05-29 16:02:03 +0300

    feat: issue status now properly updates on change

--- src/app/issues/issues.controller.js
+++ src/app/issues/issues.controller.js
@@ -3,11 +3,13 @@ import IssuesModalController from './issuesModal.controller';
 
 const _uibModal = new WeakMap();
 const _issuesService = new WeakMap();
+const _toaster = new WeakMap();
 
 export class IssuesController {
-  constructor($uibModal, IssuesService) {
+  constructor($uibModal, IssuesService, toaster) {
     _uibModal.set(this, $uibModal);
     _issuesService.set(this, IssuesService);
+    _toaster.set(this, toaster);
 
     this.issues = _issuesService.get(this).issues;
   }
@@ -19,8 +21,19 @@ export class IssuesController {
       controllerAs: 'im'
     });
   }
+
+  updateIssueStatus(issue, status) {
+    issue.status = status;
+    return this.issues.$save(issue)
+      .then(() => {
+        _toaster.get(this).pop('success', 'Status updated');
+      })
+      .catch(e => {
+        _toaster.get(this).pop('error', 'Oops', `Something went wrong ${e}`);
+      })
+  }
 }
 
-IssuesController.$inject = ['$uibModal', 'IssuesService'];
+IssuesController.$inject = ['$uibModal', 'IssuesService', 'toaster'];
 
 
