3367be3be9a31ee43040abdc223ea211a4178c1b
Sergey Khval <sergeykhval@gmail.com>
2016-05-29 13:03:03 +0300

    feat: add toaster on issue save. fix #6

--- src/app/issues/issuesModal.controller.js
+++ src/app/issues/issuesModal.controller.js
@@ -1,15 +1,23 @@
 'use strict';
 const _uibModalInstance = new WeakMap();
 const _issuesService = new WeakMap();
+const _toaster = new WeakMap();
 
 export default class IssuesModalController {
-  constructor($uibModalInstance, IssuesService) {
+  constructor($uibModalInstance, IssuesService, toaster) {
     _uibModalInstance.set(this, $uibModalInstance);
     _issuesService.set(this, IssuesService);
+    _toaster.set(this, toaster);
   }
 
   addNewIssue(issue) {
-    return _issuesService.get(this).addIssue(issue);
+    return _issuesService.get(this).addIssue(issue)
+      .then(() => {
+        _toaster.get(this).pop('success', 'New issue saved');
+      })
+      .catch(e => {
+        _toaster.get(this).pop('error', 'Oops', `Our API seems to be unavailable ${e}`);
+      });
   }
 
   cancel() {
@@ -17,4 +25,4 @@ export default class IssuesModalController {
   }
 }
 
-IssuesModalController.$inject = ['$uibModalInstance', 'IssuesService'];
+IssuesModalController.$inject = ['$uibModalInstance', 'IssuesService', 'toaster'];
