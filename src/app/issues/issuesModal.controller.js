7029c2ceac90b04451495688eebf392d47d10eed
Sergey Khval <sergeykhval@gmail.com>
2016-05-29 13:38:39 +0300

    feat: add form reset for issues form

--- src/app/issues/issuesModal.controller.js
+++ src/app/issues/issuesModal.controller.js
@@ -2,17 +2,22 @@
 const _uibModalInstance = new WeakMap();
 const _issuesService = new WeakMap();
 const _toaster = new WeakMap();
+const _scope = new WeakMap();
 
 export default class IssuesModalController {
-  constructor($uibModalInstance, IssuesService, toaster) {
+  constructor($scope, $uibModalInstance, IssuesService, toaster) {
     _uibModalInstance.set(this, $uibModalInstance);
     _issuesService.set(this, IssuesService);
     _toaster.set(this, toaster);
+    _scope.set(this, $scope);
   }
 
   addNewIssue(issue) {
     return _issuesService.get(this).addIssue(issue)
       .then(() => {
+        _scope.get(this).issue = {};
+        _scope.get(this).issueform.$setPristine();
+        _scope.get(this).issueform.$setUntouched();
         _toaster.get(this).pop('success', 'New issue saved');
       })
       .catch(e => {
@@ -25,4 +30,4 @@ export default class IssuesModalController {
   }
 }
 
-IssuesModalController.$inject = ['$uibModalInstance', 'IssuesService', 'toaster'];
+IssuesModalController.$inject = ['$scope', '$uibModalInstance', 'IssuesService', 'toaster'];
