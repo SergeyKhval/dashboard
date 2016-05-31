623d1f94411568d164aa270f0f74e6f843c8446a
Sergey Khval <sergeykhval@gmail.com>
2016-05-31 23:00:27 +0300

    feat: hardcode basic line chart

--- src/app/analytics/analytics.controller.js
+++ src/app/analytics/analytics.controller.js
@@ -1,6 +1,20 @@
 export class AnalyticsController {
-  constructor() {
-    'ngInject';
+  constructor($timeout) {
+    this.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    this.series = ['Series A', 'Series B'];
+    this.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
 
+    $timeout(() => {
+      this.data = [
+        [28, 48, 40, 19, 86, 27, 90],
+        [65, 59, 80, 81, 56, 55, 40]
+      ];
+    }, 3000);
   }
 }
+
+
+AnalyticsController.$inject = ['$timeout'];
