90879babfff8dd4e879e66c50298163641d613e3
Sergey Khval <sergeykhval@gmail.com>
2016-05-31 23:43:30 +0300

    feat: add required fields for issues

--- src/app/issues/issues.service.js
+++ src/app/issues/issues.service.js
@@ -15,7 +15,11 @@ export class IssuesService {
     let issueObj = {
       title: issue.title,
       reporter: issue.reporter,
+      description: issue.description,
+      customer: issue.customer,
+      customerEmail: issue.customerEmail,
       createdAt: Firebase.ServerValue.TIMESTAMP,
+      updatedAt: Firebase.ServerValue.TIMESTAMP,
       status: 'open'
     };
 
--- src/app/issues/issuesModal.controller.js
+++ src/app/issues/issuesModal.controller.js
@@ -2,22 +2,22 @@
 const _uibModalInstance = new WeakMap();
 const _issuesService = new WeakMap();
 const _toaster = new WeakMap();
-const _scope = new WeakMap();
 
 export default class IssuesModalController {
-  constructor($scope, $uibModalInstance, IssuesService, toaster) {
+  constructor($uibModalInstance, IssuesService, toaster) {
     _uibModalInstance.set(this, $uibModalInstance);
     _issuesService.set(this, IssuesService);
     _toaster.set(this, toaster);
-    _scope.set(this, $scope);
+
+    this.issue = {};
   }
 
   addNewIssue(issue) {
     return _issuesService.get(this).addIssue(issue)
       .then(() => {
-        _scope.get(this).issue = {};
-        _scope.get(this).issueform.$setPristine();
-        _scope.get(this).issueform.$setUntouched();
+        this.issue = {};
+        this.issueform.$setPristine();
+        this.issueform.$setUntouched();
         _toaster.get(this).pop('success', 'New issue saved');
       })
       .catch(e => {
@@ -30,4 +30,4 @@ export default class IssuesModalController {
   }
 }
 
-IssuesModalController.$inject = ['$scope', '$uibModalInstance', 'IssuesService', 'toaster'];
+IssuesModalController.$inject = ['$uibModalInstance', 'IssuesService', 'toaster'];
