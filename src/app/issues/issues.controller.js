89022f01fc05b5e39785312d1ce5efeb5ea64816
Sergey Khval <sergeykhval@gmail.com>
2016-06-26 11:42:31 +0300

    feat: add support for exporting csv files

--- src/app/issues/issues.controller.js
+++ src/app/issues/issues.controller.js
@@ -4,10 +4,12 @@ import IssuesModalController from './issuesModal.controller';
 const _uibModal = new WeakMap();
 const _issuesService = new WeakMap();
 const _toaster = new WeakMap();
+const _rootScope = new WeakMap();
 
 export class IssuesController {
-  constructor($uibModal, IssuesService, toaster) {
+  constructor($uibModal, $rootScope, IssuesService, toaster) {
     _uibModal.set(this, $uibModal);
+    _rootScope.set(this, $rootScope);
     _issuesService.set(this, IssuesService);
     _toaster.set(this, toaster);
 
@@ -16,6 +18,52 @@ export class IssuesController {
     this.search = '';
     this.filter = '';
     this.sort = 'title';
+    this.error = 'test';
+
+    //We have to set this function in the constructor
+    //If we store it in the prototype then we lose context during fn call
+    this.exportCSV = (event) => {
+      let file = event.target.files[0],
+        reader = new FileReader();
+
+      this.error = '';
+
+      reader.readAsText(file);
+
+      reader.onload = (event) => {
+        let csv = event.target.result;
+
+        let allTextLines = csv.split(/\r\n|\n/);
+
+        let result = [];
+        let headers = [];
+
+        allTextLines.forEach((line, index) => {
+          if (index === 0) {
+            headers = line.split(',');
+          } else {
+            let tempObj = {};
+            let values = line.split(',');
+
+            values.forEach((value, index) => {
+              tempObj[headers[index]] = value;
+            });
+
+            result.push(tempObj);
+          }
+        });
+
+        this.issues = result;
+        _rootScope.get(this).$apply();
+      };
+
+      reader.onerror = (evt) => {
+        if (evt.target.error.name == "NotReadableError") {
+          this.error = "Can not read file !";
+          _rootScope.get(this).$apply();
+        }
+      };
+    };
   }
 
   openAddIssueModal() {
@@ -56,7 +104,7 @@ export class IssuesController {
       })
   }
 
-  downloadCsv(){
+  downloadCsv() {
     return this.issues.map(issue => {
       return {
         title: issue.title,
@@ -69,11 +117,9 @@ export class IssuesController {
     });
   }
 
-  exportCSV(){
-    console.log('exported');
-  }
+
 }
 
-IssuesController.$inject = ['$uibModal', 'IssuesService', 'toaster'];
+IssuesController.$inject = ['$uibModal', '$rootScope', 'IssuesService', 'toaster'];
 
 
